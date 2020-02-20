071d03179fdf9840aed1fd197a9189f42f137602
David Kaiser <beckologist@hotmail.com>
2020-02-20 12:12:00 -0600

    Ture Rest Example

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 namespace GatewayAPI
 {
@@ -10,6 +12,18 @@ namespace GatewayAPI
     {
         static void Main(string[] args)
         {
+            var config = new HttpSelfHostConfiguration("http://localhost:1234");
+            config.Routes.MapHttpRoute("default",
+                                        "api/{controller}/{id}",
+                                        new { controller = "Home", id = RouteParameter.Optional });
+
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+
+            Console.WriteLine("Web API Server has started at http://localhost:1234");
+            Console.ReadLine();
         }
     }
+    
 }
